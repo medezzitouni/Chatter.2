2ea26603896e0f3377eda36bf002827a0ee9bc4d
Medpd <methe.medpd@gmail.com>
2020-10-12 14:04:49 +0100

    Core: add Facade ports

---
+++ server/Core/Ports/in/Facade/IUserFacade.js
@@ -0,0 +1,32 @@
+
+
+export default class IUserFacade{
+    add(userInstance) {
+        return Promise.reject(new Error('not implemented'));
+    }
+
+    update(userInstance) {
+        return Promise.reject(new Error('not implemented'));
+    }
+
+    delete(userInstance) {
+        return Promise.reject(new Error('not implemented'));
+    }
+
+    getById(userId) {
+        return Promise.reject(new Error('not implemented'));
+    }
+
+    getByEmail(userId) {
+        return Promise.reject(new Error('not implemented'));
+    }
+
+    getAll() {
+        return Promise.reject(new Error('not implemented'));
+    }
+
+    addRoom(userInstance, room) {
+        return Promise.reject(new Error('not implemented'));
+    }
+
+}
---
+++ server/Core/Ports/in/Facade/index.js
@@ -0,0 +1,17 @@
+import IUserFacade from './IUserFacade'
+import IMessageFacade from './IMessageFacade'
+import IRoomFacade from './IRoomFacade'
+
+
+export {
+    IUserFacade,
+    IMessageFacade,
+    IRoomFacade
+}
+
+
+export default Object.freeze({
+    IUserFacade,
+    IMessageFacade,
+    IRoomFacade
+})
