a1c4d43b488bff5ff4ff05fc59ac07de1fecb840
Medpd <methe.medpd@gmail.com>
2020-10-12 22:07:15 +0100

    Core/useCases: export use-cases

--- server/Core/useCases/index.js
+++ server/Core/useCases/index.js
@@ -2,14 +2,8 @@ import MessageFacade from './MessageFacade'
 import UserFacade from './UserFacade'
 import RoomFacade from './RoomFacade'
 
-// import { UserRepository, 
-//          RoomRepository, 
-//          MessageRepository } from '../Ports/out/DatabaseService'
 
-// const messageFacade = new MassageFacade()
-// const userFacade = new UserFacade() 
-// const roomFacade = new RoomFacade()
-  
+
 export {
     MessageFacade,
     UserFacade,
@@ -22,3 +16,4 @@ export default Object.freeze({
     UserFacade,
     RoomFacade
 })
+
