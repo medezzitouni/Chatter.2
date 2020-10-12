03b34c76e8b3479771fdfc08eef58a4ed9855875
Medpd <methe.medpd@gmail.com>
2020-10-12 22:15:55 +0100

    Core/Ports/in: instantiate the useCase classes and inject repositories

--- server/Core/Ports/in/Facade/index.js
+++ server/Core/Ports/in/Facade/index.js
@@ -1,17 +1,33 @@
 import IUserFacade from './IUserFacade'
 import IMessageFacade from './IMessageFacade'
 import IRoomFacade from './IRoomFacade'
+import { UserFacade, RoomFacade, MessageFacade } from '../../../useCases'
 
+import getRepositories from '../Ports/out/DatabaseService'
 
-export {
-    IUserFacade,
-    IMessageFacade,
-    IRoomFacade
-}
+export default ( async () => {
 
+    const repo = await getRepositories()
+    
+    // injection
+    const userFacade = UserFacade(repo.userRepository)
+    const roomFacade = RoomFacade(repo.roomRepository)
+    const messageFacade = MessageFacade(repo.messageRepository)
 
-export default Object.freeze({
-    IUserFacade,
-    IMessageFacade,
-    IRoomFacade
-})
+    if(!( userFacade instanceof IUserFacade ))
+        reject(new Error("the userFacade doesn't implement the IUserFacade"))
+
+    if(!( roomFacade instanceof IRoomFacade ))
+        reject(new Error("the roomFacade doesn't implement the IRoomFacade"))
+
+    if(!( messageFacade instanceof IMessageFacade ))
+        reject(new Error("the messageFacade doesn't implement the IMessageFacade"))
+
+      
+    return Object.freeze ({
+        userFacade,
+        roomFacade,
+        messageFacade  
+    });
+})();
+
