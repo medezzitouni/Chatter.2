9c1543338e3a6070cf88a8ddfd3f5626c1f6ba2a
Medpd <methe.medpd@gmail.com>
2020-10-20 21:41:57 +0100

    Models: delete all models

--- server/App/Models/User/index.js
+++
@@ -1,9 +0,0 @@
-import createLoginModel from './LoginModel'
-import { hashAdapter, sanitizeAdapter } from '../adapters'
-
-
-const LoginModel = createLoginModel({sanitize: sanitizeAdapter})
-
-export {
-    LoginModel
-}
--- server/App/Models/adapters/sanitize.adapter.js
+++
@@ -1,9 +0,0 @@
-import { sanitizeAdapter } from ".";
-
-
-
-
-export default function sanitize(text){
-
-    return `${text} sanitized`
-}
